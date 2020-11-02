82da6bd4fa9a0d3bf5b164e48fcd1515c56f2a96
aigeal-eight <zeradt@gmail.com>
2020-11-02 16:19:37 +0545

    optimized the module workflow

--- preprocessor/preprocessor.py
+++ preprocessor/preprocessor.py
@@ -1,123 +1,3 @@
-to_replace = {"ain't": "am not",
-"aren't": "are not",
-"can't": "cannot",
-"can't've": "cannot have",
-"'cause": "because",
-"could've": "could have",
-"couldn't": "could not",
-"couldn't've": "could not have",
-"didn't": "did not",
-"doesn't": "does not",
-"don't": "do not",
-"hadn't": "had not",
-"hadn't've": "had not have",
-"hasn't": "has not",
-"haven't": "have not",
-"he'd": "he had",
-"he'd've": "he would have",
-"he'll": "he will",
-"he'll've": "he will have",
-"he's": "he is",
-"how'd": "how did",
-"how'd'y": "how do you",
-"how'll": "how will",
-"how's": "how is",
-"I'd": "I had",
-"I'd've": "I would have",
-"I'll": "I will",
-"I'll've": "I will have",
-"I'm": "I am",
-"I've": "I have",
-"isn't": "is not",
-"it'd": "it had",
-"it'd've": "it would have",
-"it'll": "it will",
-"it'll've": "iit will have",
-"it's": "it is",
-"let's": "let us",
-"ma'am": "madam",
-"mayn't": "may not",
-"might've": "might have",
-"mightn't": "might not",
-"mightn't've": "might not have",
-"must've": "must have",
-"mustn't": "must not",
-"mustn't've": "must not have",
-"needn't": "need not",
-"needn't've": "need not have",
-"o'clock": "of the clock",
-"oughtn't": "ought not",
-"oughtn't've": "ought not have",
-"shan't": "shall not",
-"sha'n't": "shall not",
-"shan't've": "shall not have",
-"she'd": "she had",
-"she'd've": "she would have",
-"she'll": "she will",
-"she'll've": "she will have",
-"she's": "she is",
-"should've": "should have",
-"shouldn't": "should not",
-"shouldn't've": "should not have",
-"so've": "so have",
-"so's": "so is",
-"that'd": "that had",
-"that'd've": "that would have",
-"that's": "that is",
-"there'd": "there had",
-"there'd've": "there would have",
-"there's": "there is",
-"they'd": "they had",
-"they'd've": "they would have",
-"they'll": "they will",
-"they'll've": "they will have",
-"they're": "they are",
-"they've": "they have",
-"to've": "to have",
-"wasn't": "was not",
-"we'd": "we had",
-"we'd've": "we would have",
-"we'll": "we will",
-"we'll've": "we will have",
-"we're": "we are",
-"we've": "we have",
-"weren't": "were not",
-"what'll": "what will",
-"what'll've": "what will have",
-"what're": "what are",
-"what's": "what is",
-"what've": "what have",
-"when's": "when is",
-"when've": "when have",
-"where'd": "where did",
-"where's": "where is",
-"where've": "where have",
-"who'll": "who will",
-"who'll've": "who will have",
-"who's": "who is",
-"who've": "who have",
-"why's": "why is",
-"why've": "why have",
-"will've": "will have",
-"won't": "will not",
-"won't've": "will not have",
-"would've": "would have",
-"wouldn't": "would not",
-"wouldn't've": "would not have",
-"y'all": "you all",
-"y'all'd": "you all would",
-"y'all'd've": "you all would have",
-"y'all're": "you all are",
-"y'all've": "you all have",
-"you'd": "you had",
-"you'd've": "you would have",
-"you'll": "you will",
-"you'll've": "you will have",
-"you're": "you are",
-"you've": "you have"}
-
-
-
 import re
 import unidecode
 from nltk.tokenize import word_tokenize, sent_tokenize
@@ -127,13 +7,15 @@ from nltk.corpus import stopwords
 from pycontractions import Contractions
 from autocorrect import Speller
 from file_reader import prepare_text
+from preprocessor.contractions import to_replace
+from gensim import downloader as api
 
 import os
 java_path = "C:/Program Files/Java/jdk1.8.0_261/bin/java.exe"
 os.environ['JAVAHOME'] = java_path
 
 class PreProcessor():
-    def __init__(self, file_path=None,remove_stopwords=True, lower=True, tokenize_word=True, tokenize_sent=False,contraction_method='mapping',
+    def __init__(self, file_path=None,remove_stopwords=True, lower=True, tokenize_word=True,contraction_method='mapping',
                  remove_numbers=True, remove_html_tags=True,
                  remove_punctuations=True, remove_accented_chars=True, remove_whitespace=True,
                  lemmatize_method='wordnet',
@@ -150,7 +32,7 @@ class PreProcessor():
         if (type(remove_stopwords) != bool or
             type(lower) != bool or
             type(tokenize_word) != bool or
-            type(tokenize_sent) != bool or
+            # type(tokenize_sent) != bool or
             type(remove_numbers) != bool or
             type(remove_html_tags) != bool or
             type(remove_punctuations) != bool or
@@ -170,7 +52,10 @@ class PreProcessor():
             raise Exception("Error - embedding method not supported")
         else:
             self.word_embedding = True
+        if file_path == None:
+            raise Exception("Error - expecting the file path")
         self.doc = None
+        self.sents = None
         self.tweets = None
         self.lemmatizer = None
         self.file_path = file_path
@@ -187,15 +72,15 @@ class PreProcessor():
         self.stopword_list = stopwords.words('english')
         self.replacement_list = to_replace
         self.tokenize_word = tokenize_word
-        self.tokenize_sent = tokenize_sent
+        # self.tokenize_sent = tokenize_sent
         self.auto_correct = auto_correct
         if self.lemmatize_method == 'wordnet':
             self.lemmatizer = WordNetLemmatizer()
         if self.lemmatize_method == 'snowball':
             self.lemmatizer = SnowballStemmer('english')
     
-    def file_reader(self,file_path):
-        file_content = prepare_text(file_path, dolower=False)
+    def file_reader(self):
+        file_content = prepare_text(self.file_path, dolower=False)
         return file_content
 
     def lower_fun(self):
@@ -217,8 +102,18 @@ class PreProcessor():
         self.doc = ' '.join(cleaned_tokens)
 
     def word_embedding_fun(self):
+        # if(self.tokenize_sent==False):
+        #     self.doc = sent_tokenize(self.doc)
+        if(self.tokenize_word==False):
+            self.tokenize_word_fun()
         if self.embedding_method == 'glove':
-            pass            
+            model = api.load("glove-twitter-25")
+            vecs=[]
+            for x in self.doc:
+                vec = [model[i] for i in x]
+                vecs.append(vec)
+                self.doc = vecs
+            # print(vecs)
         elif self.embedding_method == 'word2vec':
             pass
         elif self.embedding_method == 'bow':
@@ -249,7 +144,6 @@ class PreProcessor():
             cont.load_models()
             self.doc = list(cont.expand_texts([str(self.doc)],precise=True))[0]
         elif self.contraction_method == 'glove':
-            import gensim.downloader as api
             model = api.load("glove-twitter-25")
             cont = Contractions(kv_model=model)
             cont.load_models()
@@ -301,9 +195,9 @@ class PreProcessor():
         """tokenizes the sentences to words"""
         self.doc = word_tokenize(self.doc)
     
-    def tokenize_sent_fun(self):
-        """tokenizes the paragraphs to sentences"""
-        self.doc = sent_tokenize(self.doc)
+    # def tokenize_sent_fun(self):
+    #     """tokenizes the paragraphs to sentences"""
+    #     self.sents = sent_tokenize(self.doc)
 
     def lemmatize_fun(self):
         """
@@ -319,7 +213,7 @@ class PreProcessor():
         cleaned_tokens = None
         if self.lemmatize_method == 'wordnet':
             cleaned_tokens = [self.lemmatizer.lemmatize(token) for token in self.doc]
-        else:
+        elif self.lemmatize_method == 'snowball':
             cleaned_tokens = [self.lemmatizer.stem(token) for token in self.doc]
        
         self.doc = ' '.join(cleaned_tokens)
@@ -355,7 +249,7 @@ class PreProcessor():
             raise Exception("Error - stopword list is empty")
         print(self.stopword_list)
 
-    def process(self, doc):
+    def process(self):
         """
         This function processes the doc
         If the remove_stopwords flag is True
@@ -372,33 +266,35 @@ class PreProcessor():
         obj = NLP()
         df = df['text].apply(obj.process)
         """
-        self.doc = doc
-        # if self.file_path is True:
-        #     self.file_reader()
-        if self.tokenize_sent is True:
-            self.tokenize_sent_fun()
-        if self.lower is True:
-            self.lower_fun()
-        if self.contractions is True:
-            self.contractions_fun()
-        if self.remove_html_tags is True:
-            self.remove_html_tags_fun()
-        if self.remove_numbers is True:
-            self.remove_numbers_fun()
-        if self.remove_punctations is True:
-            self.remove_punctations_fun()
-        if self.remove_accented_chars is True:
-            self.remove_accented_chars_fun()
-        if self.remove_stopwords is True:
-            self.remove_stopwords_fun()
-        if self.remove_whitespace is True:
-            self.remove_whitespace_fun()
-        if self.auto_correct is True:
-            self.autocorrect_fun()
-        if self.lemmatize is True:
-            self.lemmatize_fun()
-        if self.tokenize_word is True:
-            self.tokenize_word_fun()
-        if self.word_embedding is True:
-            self.word_embedding_fun()
-        return self.doc
+        if self.file_path != None:
+            data = self.file_reader()
+        output=[]
+        self.sents = sent_tokenize(data)
+        for doc in self.sents:
+            self.doc = doc
+            if self.lower is True:
+                self.lower_fun()
+            if self.contractions is True:
+                self.contractions_fun()
+            if self.remove_html_tags is True:
+                self.remove_html_tags_fun()
+            if self.remove_numbers is True:
+                self.remove_numbers_fun()
+            if self.remove_punctations is True:
+                self.remove_punctations_fun()
+            if self.remove_accented_chars is True:
+                self.remove_accented_chars_fun()
+            if self.remove_stopwords is True:
+                self.remove_stopwords_fun()
+            if self.remove_whitespace is True:
+                self.remove_whitespace_fun()
+            if self.auto_correct is True:
+                self.autocorrect_fun()
+            if self.lemmatize is True:
+                self.lemmatize_fun()
+            if self.tokenize_word is True:
+                self.tokenize_word_fun()
+            if self.word_embedding is True:
+                self.word_embedding_fun()
+            output.append(self.doc)
+        return output
--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 from preprocessor import PreProcessor
 from nltk.tokenize import sent_tokenize
 
+filePath = "C:/Users/zerad/Desktop/sujan/git_repo/preprocessor/news.docx"
+
 prepObj = PreProcessor(
+       file_path=filePath,
        lower=True,
-       tokenize_word=True,
-       contraction_method='mapping',
+       tokenize_word=False, #if false the output will be in list of sentences
        remove_stopwords=True,
        remove_numbers=True,
        remove_html_tags=True,
@@ -12,10 +14,11 @@ prepObj = PreProcessor(
        remove_accented_chars=True,
        remove_whitespace=True,
        auto_correct=True,
-       lemmatize_method='wordnet',
-       embedding_method='word2vec'
+       lemmatize_method='snowball',
+       embedding_method='word2vec',
+       contraction_method='mapping',
       )
-contents = prepObj.file_reader("C:/Users/zerad/Desktop/sujan/git_repo/preprocessor/news.docx")
-example = sent_tokenize(contents)
-preprocessed = [prepObj.process(i) for i in example]
+
+preprocessed = prepObj.process()
+
 print(preprocessed)
